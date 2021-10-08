960deff84db85054d6268988dc9f4bb4f2806d86
Diogo Corigo <diogoacorigo@gmail.com>
2021-10-08 12:25:56 +0100

    added files

---
+++ Windows Service Template/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace Windows_Service_Template
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var exitCode = HostFactory.Run(x =>
+      {
+        x.Service<ServiceCode>(s =>
+        {
+          s.ConstructUsing(service => new ServiceCode());
+          s.WhenStarted(service => service.Start());
+          s.WhenStopped(service => service.Stop());
+        });
+
+        x.RunAsLocalSystem();
+
+        x.SetServiceName("ServiceName");
+        x.SetDisplayName("Service Name");
+        x.SetDescription("Service Description");
+      });
+
+      int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+      Environment.ExitCode = exitCodeValue;
+
+      // Install Service:
+        // Copy files in bin/debug to a permanent folder
+        // Open terminal, as administrator, on that folder
+        // Run FileName.exe intall start
+      // Uninstall Service:
+        // Open folder where files are
+        // Open terminal, as administrator, on that folder
+        // Run FileName.exe uninstall
+    }
+  }
+}
---
+++ Windows Service Template/ServiceCode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Windows_Service_Template
+{
+  class ServiceCode
+  {
+    private readonly Timer _timer;
+
+    public ServiceCode()
+    {
+      _timer = new Timer(1000) { AutoReset = true };
+      _timer.Elapsed += TimerElapsed;
+    }
+
+    private void TimerElapsed(object sender, ElapsedEventArgs e)
+    {
+      // Add code here
+      throw new NotImplementedException();
+    }
+
+    public void Start()
+    {
+      _timer.Start();
+    }
+
+    public void Stop()
+    {
+      _timer.Stop();
+    }
+  }
+}
